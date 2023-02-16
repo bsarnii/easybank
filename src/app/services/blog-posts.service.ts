f4e71a536e79a0fd180a0b9f69358f18829494dc
bsarnii <69685787+bsarnii@users.noreply.github.com>
2023-02-16 17:38:14 +0100

    Update blog-posts.service.ts

--- src/app/services/blog-posts.service.ts
+++ src/app/services/blog-posts.service.ts
@@ -8,7 +8,7 @@ import { Posts } from '../types/http-posts.interface';
 })
 export class BlogPostsService {
   private apiUrl= "https://trw-news.p.rapidapi.com/";
-  private apiKey= process.env.POSTS_API_KEY;
+  private apiKey= process.env['POSTS_API_KEY'];
   private apiHost= "trw-news.p.rapidapi.com";
   private params= {
     q: "(language:en)AND(site_country:uk)AND(text:economy banking)AND(site:warrington-worldwide.co.uk OR newscabal.co.uk)",
