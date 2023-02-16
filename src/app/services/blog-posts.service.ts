b6bedce3454ba32e3428eaf4600cd7209cb576ff
bsarnii <69685787+bsarnii@users.noreply.github.com>
2023-02-16 17:51:54 +0100

    Update blog-posts.service.ts

--- src/app/services/blog-posts.service.ts
+++ src/app/services/blog-posts.service.ts
@@ -2,13 +2,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Posts } from '../types/http-posts.interface';
+import { environment } from '../../environment.ts'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogPostsService {
   private apiUrl= "https://trw-news.p.rapidapi.com/";
-  private apiKey= process.env['POSTS_API_KEY'];
+  private apiKey= environment.POSTS_API_KEY
   private apiHost= "trw-news.p.rapidapi.com";
   private params= {
     q: "(language:en)AND(site_country:uk)AND(text:economy banking)AND(site:warrington-worldwide.co.uk OR newscabal.co.uk)",
