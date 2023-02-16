741e2420a678c0d318d15dc0b3f7e36c471679d7
bsarnii <69685787+bsarnii@users.noreply.github.com>
2023-02-16 17:02:43 +0100

    Update http-jobs.service.ts

--- src/app/services/http-jobs.service.ts
+++ src/app/services/http-jobs.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Job } from '../types/http-job.interface';
 import { Observable } from "rxjs";
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +12,7 @@ export class HttpJobsService {
   private params = 
     {
       "app_id": "5285e84f",
-      "app_key": environment.jobsApiKey,
+      "app_key": process.env.jobsApiKey,
       "results_per_page": 9,
       "title_only": "software",
       "where": 99423,
