b5b4ee4090feff7d979a202f670f033f5a746949
bsarnii <69685787+bsarnii@users.noreply.github.com>
2023-02-16 18:24:36 +0000

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  JOBS_API_KEY: process.env["JOBS_API_KEY"] || "asd",
-  POSTS_API_KEY: process.env["POSTS_API_KEY"] || "asd",
+  JOBS_API_KEY: process.env["JOBS_API_KEY"] || "b9866df9252fa5ee21daa43fee05810f",
+  POSTS_API_KEY: process.env["POSTS_API_KEY"] || "d4e2b9aac1msh56cc676aef23befp1c72dfjsnc28a6b36a350",
 };
