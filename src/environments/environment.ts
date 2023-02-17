157ab39765cf2601b320aac031d1839a8077019d
bsarnii <scharnoui@gmail.com>
2023-02-17 13:13:24 +0100

    change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    postsApiKey: "d4e2b9aac1msh56cc676aef23befp1c72dfjsnc28a6b36a350",
-    jobsApiKey: "b9866df9252fa5ee21daa43fee05810f"
+    postsApiKey: process.env["postsApiKey"],
+    jobsApiKey: process.env["jobsApiKey"]
   };
