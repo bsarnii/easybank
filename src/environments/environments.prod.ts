1913e904f6376b2afbc836daa10794ee1cff0956
bsarnii <scharnoui@gmail.com>
2023-02-17 13:15:06 +0100

    chasnges

--- src/environments/environments.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    JOBS_API_KEY: "b9866df9252fa5ee21daa43fee05810f",
-    POSTS_API_KEY: "d4e2b9aac1msh56cc676aef23befp1c72dfjsnc28a6b36a350",
-  };
