deb656dcaf6a5ad273e1b8005df7f3d31485766d
bsarnii <scharnoui@gmail.com>
2023-04-09 14:32:43 +0200

    removed time limit from posts API request

--- src/app/services/blog-posts.service.ts
+++ src/app/services/blog-posts.service.ts
@@ -13,7 +13,6 @@ export class BlogPostsService {
   private apiHost= "trw-news.p.rapidapi.com";
   private params= {
     q: "(language:en)AND(site_country:uk)AND(text:economy banking)AND(site:warrington-worldwide.co.uk OR newscabal.co.uk)",
-    ts: 1675159335000,
     size: 4,
     "rapidapi-key": this.apiKey
   }
